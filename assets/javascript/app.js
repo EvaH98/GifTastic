ff4702f3977966de6e50c1b106d550d9a5c6aac9
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-03-06 16:28:28 -0700

    Added ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,4 +8,11 @@ function displayMoodGifs() {
 	var mood = $(this).attr("data-name");
 	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=nRm8yx7PNmCAqAcIMSRMnye9fP48tV4y" + mood + "&limit=10&offset=0&lang=en";
 
+	//ajax call 
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).then(function(respnse) {
+
+	});
 } 
