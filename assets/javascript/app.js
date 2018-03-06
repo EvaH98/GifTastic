d040a43d17f748f3375c3f24f207daa073acd3ed
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-03-06 16:20:07 -0700

    Added some javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//Initial Array of moods
+
+var moods = ["Happy", "Sad", "Angry", "Hungry"]
+
+//This function will re-render the HTML to display the appropriate content
+function displayMoodGifs() {
+
+	var mood = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=nRm8yx7PNmCAqAcIMSRMnye9fP48tV4y" + mood + "&limit=10&offset=0&lang=en";
+
+} 
